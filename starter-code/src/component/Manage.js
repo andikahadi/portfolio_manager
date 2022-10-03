8ce54d448d76e96c22d0ab37285156e699a3e4a4
andikahadisaputra <andikahadisaputra@gmail.com>
2022-10-03 15:38:20 +0800

    moving fetch function to parent

--- starter-code/src/component/Manage.js
+++ starter-code/src/component/Manage.js
@@ -11,57 +11,57 @@ const Manage = (props) => {
 
   let isMounted = useRef(false);
 
-  const fetchInfo = async (url1, url2, signal) => {
-    setIsLoading(true);
-    setError(false);
+  // const fetchInfo = async (url1, url2, stateInput, changeOutFn, signal) => {
+  //   setIsLoading(true);
+  //   setError(false);
 
-    try {
-      const res1 = await fetch(url1, { signal: signal });
-      const res2 = await fetch(url2, { signal: signal });
+  //   try {
+  //     const res1 = await fetch(url1, { signal: signal });
+  //     const res2 = await fetch(url2, { signal: signal });
 
-      if (res1.status !== 200 || res2.status !== 200) {
-        throw new Error("Something went wrong");
-      }
-      const data1 = await res1.json();
-      const data2 = await res2.json();
+  //     if (res1.status !== 200 || res2.status !== 200) {
+  //       throw new Error("Something went wrong");
+  //     }
+  //     const data1 = await res1.json();
+  //     const data2 = await res2.json();
 
-      const nameRemoveInc = data1.name.slice(0, -4);
-      const twoDecUnrealizedGain =
-        Math.round(
-          parseInt(newHolding.position) *
-            (data2.c - parseInt(newHolding.cost)) *
-            100
-        ) / 100;
+  //     const nameRemoveInc = data1.name.slice(0, -4);
+  //     const twoDecUnrealizedGain =
+  //       Math.round(
+  //         parseInt(stateInput.position) *
+  //           (data2.c - parseInt(stateInput.cost)) *
+  //           100
+  //       ) / 100;
 
-      const twoDecValue =
-        Math.round(parseInt(newHolding.position) * data2.c * 100) / 100;
+  //     const twoDecValue =
+  //       Math.round(parseInt(stateInput.position) * data2.c * 100) / 100;
 
-      const stockInfo = {
-        symbol: newHolding.symbol.toUpperCase(),
-        name: nameRemoveInc,
-        logo: data1.logo,
-        position: newHolding.position,
-        cost: newHolding.cost,
-        price: data2.c,
-        value: twoDecValue,
-        unrealizedGain: twoDecUnrealizedGain,
-        unrealizedGainPct:
-          (Math.round(
-            (twoDecUnrealizedGain / (newHolding.position * newHolding.cost)) *
-              1000
-          ) /
-            1000) *
-          100,
-      };
+  //     const stockInfo = {
+  //       symbol: stateInput.symbol.toUpperCase(),
+  //       name: nameRemoveInc,
+  //       logo: data1.logo,
+  //       position: stateInput.position,
+  //       cost: stateInput.cost,
+  //       price: data2.c,
+  //       value: twoDecValue,
+  //       unrealizedGain: twoDecUnrealizedGain,
+  //       unrealizedGainPct:
+  //         (Math.round(
+  //           (twoDecUnrealizedGain / (stateInput.position * stateInput.cost)) *
+  //             1000
+  //         ) /
+  //           1000) *
+  //         100,
+  //     };
 
-      console.log(data2);
-      props.addHoldings(stockInfo);
-    } catch (err) {
-      setError(err.message);
-    }
+  //     console.log(data2);
+  //     changeOutFn(stockInfo);
+  //   } catch (err) {
+  //     setError(err.message);
+  //   }
 
-    setIsLoading(false); // turn off loading animation
-  };
+  //   setIsLoading(false); // turn off loading animation
+  // };
 
   const handleSubmit = (input) => {
     setNewHolding(input);
@@ -80,7 +80,13 @@ const Manage = (props) => {
         "&token=ccprl9aad3idf7jqketgccprl9aad3idf7jqkeu0";
 
       const controller = new AbortController();
-      fetchInfo(urlInfo, urlPrice, controller.signal);
+      props.fetchInfo(
+        urlInfo,
+        urlPrice,
+        newHolding,
+        props.addHoldings,
+        controller.signal
+      );
       console.log(urlPrice);
       return () => {
         controller.abort();
--- starter-code/src/component/StateContainer.js
+++ starter-code/src/component/StateContainer.js
@@ -1,13 +1,71 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Manage from "./Manage";
 
 const StateContainer = () => {
-  const initHoldings = [
-    // { symbol: "aapl", position: 25, cost: 132 },
-    // { symbol: "fb", position: 30, cost: 92 },
-    // { symbol: "amzn", position: 3, cost: 1500 },
-  ];
-  const [holdings, setHoldings] = useState(initHoldings);
+  const [holdings, setHoldings] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const [temp, setTemp] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setTemp((prevTemp) => prevTemp + 1);
+    }, 60000);
+  }, []);
+
+  const fetchInfo = async (url1, url2, stateInput, changeOutFn, signal) => {
+    setIsLoading(true);
+    setError(false);
+
+    try {
+      const res1 = await fetch(url1, { signal: signal });
+      const res2 = await fetch(url2, { signal: signal });
+
+      if (res1.status !== 200 || res2.status !== 200) {
+        throw new Error("Something went wrong");
+      }
+      const data1 = await res1.json();
+      const data2 = await res2.json();
+
+      const nameRemoveInc = data1.name.slice(0, -4);
+      const twoDecUnrealizedGain =
+        Math.round(
+          parseInt(stateInput.position) *
+            (data2.c - parseInt(stateInput.cost)) *
+            100
+        ) / 100;
+
+      const twoDecValue =
+        Math.round(parseInt(stateInput.position) * data2.c * 100) / 100;
+
+      const stockInfo = {
+        symbol: stateInput.symbol.toUpperCase(),
+        name: nameRemoveInc,
+        logo: data1.logo,
+        position: stateInput.position,
+        cost: stateInput.cost,
+        price: data2.c,
+        value: twoDecValue,
+        unrealizedGain: twoDecUnrealizedGain,
+        unrealizedGainPct:
+          (Math.round(
+            (twoDecUnrealizedGain / (stateInput.position * stateInput.cost)) *
+              1000
+          ) /
+            1000) *
+          100,
+      };
+
+      console.log(data2);
+      changeOutFn(stockInfo);
+    } catch (err) {
+      setError(err.message);
+    }
+
+    setIsLoading(false); // turn off loading animation
+  };
 
   const addHoldings = (input) => {
     setHoldings((prevState) => [...prevState, input]);
@@ -28,9 +86,12 @@ const StateContainer = () => {
       return arr;
     });
   };
+
   return (
     <>
+      {console.log(temp)}
       <Manage
+        fetchInfo={fetchInfo}
         holdings={holdings}
         addHoldings={addHoldings}
         handleUpdateEntry={handleUpdateEntry}
