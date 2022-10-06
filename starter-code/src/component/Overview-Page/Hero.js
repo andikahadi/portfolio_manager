f674083b3cf926d4f8d24901e17ff72e49ff86f0
andikahadisaputra <andikahadisaputra@gmail.com>
2022-10-06 17:01:36 +0800

    add total value at center of chart

--- starter-code/src/component/Overview-Page/Hero.js
+++ starter-code/src/component/Overview-Page/Hero.js
@@ -3,8 +3,32 @@ import Chart from "./Chart";
 
 const Hero = (props) => {
   return (
-    <div>
+    <div className="hero">
       <Chart holdings={props.holdings} totalValue={props.totalValue} />
+      <div className="total-value">
+        <h3>${props.totalValue}</h3>
+        {props.totalGain > 0 ? (
+          <div className="gain-positive">
+            <label>${props.totalGain} </label>
+            <div className="icon-positive icon "></div>
+            <label>
+              {Math.round((props.totalGain / props.totalValue) * 100 * 100) /
+                100}
+              %
+            </label>
+          </div>
+        ) : (
+          <div className="gain-negative">
+            <label>-${Math.abs(props.totalGain)} </label>
+            <div className="icon-negative icon "></div>
+            <label>
+              {Math.round((props.totalGain / props.totalValue) * 100 * 100) /
+                100}
+              %
+            </label>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- starter-code/src/component/Overview-Page/Overview.js
+++ starter-code/src/component/Overview-Page/Overview.js
@@ -4,15 +4,21 @@ import Portfolio from "./Portfolio";
 
 const Overview = (props) => {
   let totalValue = 0;
+  let totalGain = 0;
 
   for (let i = 0; i < props.holdings.length; i++) {
     totalValue = totalValue + props.holdings[i].value;
+    totalGain = totalGain + props.holdings[i].unrealizedGain;
   }
   console.log(totalValue);
   return (
     <div className="overview-container">
       <h2>MyPortfolio</h2>
-      <Hero holdings={props.holdings} totalValue={totalValue} />
+      <Hero
+        holdings={props.holdings}
+        totalValue={totalValue}
+        totalGain={totalGain}
+      />
       <Portfolio holdings={props.holdings} totalValue={totalValue} />
     </div>
   );
